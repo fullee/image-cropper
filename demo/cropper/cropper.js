0dac9dd1b9dfe5af54ccdb654cb80cf39cbfe093
fullee <fullee@tencent.com>
2022-11-14 14:52:50 +0800

    字节小程序

--- demo/cropper/cropper.js
+++ demo/cropper/cropper.js
@@ -11,48 +11,41 @@ Page({
         disable_ratio: false, //锁定比例
         limit_move: true, //是否限制移动
     },
-    onLoad: function (options) {
+    onShow: function (options) {
         this.cropper = this.selectComponent("#image-cropper");
-        this.setData({
-            src: options.imgSrc
-        });
-        if (!options.imgSrc) {
-            this.cropper.upload(); //上传图片
+        if (!this.data.src) {
+            let that = this;
+            tt.chooseImage({
+                count: 1,
+                sizeType: ['original', 'compressed'],
+                sourceType: ['album', 'camera'],
+                success(res) {
+                    tt.showLoading({ title: '加载中' })
+                    const tempFilePaths = res.tempFilePaths[0];
+                    //重置图片角度、缩放、位置
+                    that.cropper.imgReset();
+                    that.setData({
+                        src: tempFilePaths
+                    });
+                    tt.hideLoading();
+                }
+            })
         }
     },
     cropperload(e) {
         console.log('cropper加载完成');
     },
     loadimage(e) {
-        wx.hideLoading();
+        tt.hideLoading();
         console.log('图片');
         this.cropper.imgReset();
     },
     clickcut(e) {
         console.log(e.detail);
         //图片预览
-        wx.previewImage({
+        tt.previewImage({
             current: e.detail.url, // 当前显示图片的http链接
             urls: [e.detail.url] // 需要预览的图片http链接列表
-        })
-    },
-    upload() {
-        let that = this;
-        wx.chooseImage({
-            count: 1,
-            sizeType: ['original', 'compressed'],
-            sourceType: ['album', 'camera'],
-            success(res) {
-                wx.showLoading({
-                    title: '加载中',
-                })
-                const tempFilePaths = res.tempFilePaths[0];
-                //重置图片角度、缩放、位置
-                that.cropper.imgReset();
-                that.setData({
-                    src: tempFilePaths
-                });
-            }
         })
     },
     setWidth(e) {
@@ -128,7 +121,8 @@ Page({
     submit() {
         this.cropper.getImg((obj) => {
             app.globalData.imgSrc = obj.url;
-            wx.navigateBack({
+            console.log(obj)
+            tt.navigateBack({
                 delta: -1
             })
         });
